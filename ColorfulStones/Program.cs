d787c9fa59978050a14fccf42a717055e76256f6
unknown <islammohamedahmed49@gmail.com>
2024-03-09 13:54:46 +0200

    colorful stones

---
+++ ColorfulStones/Program.cs
@@ -0,0 +1,17 @@
+﻿string str1, str2;
+int count = 0,indicator = 0;
+str1 = Console.ReadLine();
+str2 = Console.ReadLine();
+for(int i = 0; i < str2.Length; i++)
+{
+    if (str1[indicator] != str2[i])
+    {
+        count++;
+    }
+    else
+    {
+        indicator++;
+
+    }
+}
+Console.WriteLine(indicator+1);
